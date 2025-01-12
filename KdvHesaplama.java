a6ba7d857af5a915cf335f049b222513a4f99308
Emre <65902526+EmreDr@users.noreply.github.com>
2025-01-12 03:10:31 +0300

    Create KdvHesaplama.java
    Kullanıcıdan alınan para değerine göre KDV tutarını ve KDV'li fiyatı hesaplayan program

---
+++ KdvHesaplama.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Lütfen işlem tutaeınızı giriniz : ");
+        double tutar = input.nextDouble();
+
+        double kdvOrani = ( tutar>0 && tutar<1000) ? 0.18 : 0.08;
+
+        double kdvTutari = tutar * kdvOrani;
+        double kdvFiyati = tutar + kdvTutari;
+
+
+        System.out.println("KDV'siz Fiyat: " + tutar);
+        System.out.println("KDV Oranı: %" + (kdvOrani * 100));
+        System.out.println("KDV Tutarı: " + kdvTutari);
+        System.out.println("KDV'li Fiyat: " + kdvFiyati);
+
+    }
+}
